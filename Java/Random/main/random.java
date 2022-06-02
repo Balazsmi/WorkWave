990a322f4da0bf4ff78c18c5af37aa3c9a691873
Balazsmi <b.miskey2005@gmail.com>
2022-06-02 17:23:09 +0200

    1

--- Java/Random/main/random.java
+++ Java/Random/main/random.java
@@ -1,12 +1,27 @@
+import java.util.Arrays;
 public class random {
+
   public static String[] list = new String[] {"Camren Bonilla","Walker Armstrong","Louis Fields","Alia Tapia","Preston Coffey","Nia Goodwin","Leroy Castaneda","Zaiden Frank","Trenton Duarte","Leticia Hardy","Blaze Lindsey","Miranda Chambers"};
+  public static String[] list2 = new String[] {};
 
 
   public static void main(String[] args) {
-    for (int i = 0; i < list.length; i++) {
-      System.out.println(list[i]);
-    }
+    printList();
+    shouffle(list.length);
+  }
 
+  public static void printList() {
+    System.out.println("List: " + Arrays.toString(list));
+  }
+
+  public static void shouffle(int max) {
+    int random = (int)Math.floor(Math.random()*(max-0));
+    System.out.println(random);
+
+
+    for (int i = 0; i < list.length; i++) {
+
+    }
   }
 
 }
