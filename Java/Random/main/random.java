9c977693ca1add4387dc74786ff1a1cd1c98b393
Balazsmi <b.miskey2005@gmail.com>
2022-06-02 17:09:55 +0200

    0.1

--- Java/Random/main/random.java
+++ Java/Random/main/random.java
@@ -1,5 +1,12 @@
 public class random {
+  public static String[] list = new String[] {"Camren Bonilla","Walker Armstrong","Louis Fields","Alia Tapia","Preston Coffey","Nia Goodwin","Leroy Castaneda","Zaiden Frank","Trenton Duarte","Leticia Hardy","Blaze Lindsey","Miranda Chambers"};
+
+
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    for (int i = 0; i < list.length; i++) {
+      System.out.println(list[i]);
+    }
+
   }
+
 }
