3c7be0511535158344583ae095473082c06a4489
Balazsmi <b.miskey2005@gmail.com>
2022-12-02 21:00:59 +0100

    Random java file for temporary tests

--- Java/Random/main/random.java
+++ Java/Random/main/random.java
@@ -1,24 +1,5 @@
-import java.util.Arrays;
-public class random {
-
-  public static String[] list = new String[] {"Camren Bonilla","Walker Armstrong","Louis Fields","Alia Tapia","Preston Coffey","Nia Goodwin","Leroy Castaneda","Zaiden Frank","Trenton Duarte","Leticia Hardy","Blaze Lindsey","Miranda Chambers"};
-  public static String[] list2 = new String[] {};
-
-
-  public static void main(String[] args) {
-    printList();
-    shouffle();
-  }
-
-  public static void printList() {
-    System.out.println("List: " + Arrays.toString(list));
-  }
-
-  public static void shouffle() {
-
-    for (int i = 0; i < list.length; i++) {
-      System.out.println((int)Math.floor(Math.random() * (list.length + 1)));
+public class Random {
+    public static void main(String[] args) {
+        System.out.println("Random");
     }
-  }
-
 }
