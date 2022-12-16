2fcf4ec15f55afe005eb5a503ecf6773d3d1fb5d
Balazsmi <b.miskey2005@gmail.com>
2022-12-16 23:20:16 +0100

    new design

--- WPA/Memento/main.js
+++ WPA/Memento/main.js
@@ -16,13 +16,13 @@ function plus() {
 var pathname = window.location.pathname;
 switch(pathname) {
     case "/WPA/Memento/home.html" :
-        document.getElementById("home").style.color = "var(--font)";
+        document.getElementById("indicator-home").style.background = "visible";
         break;
     case "/WPA/Memento/list.html" :
-        document.getElementById("list").style.color = "var(--font)";
+        document.getElementById("indicator-list").style.visibility = "visible";
         break;
     case "/WPA/Memento/plus.html" :
-        document.getElementById("plus").style.color = "var(--font)";
+        document.getElementById("indicator-plus").style.color = "visible";
         break;
 
 }
@@ -31,21 +31,22 @@ let when = 'monthly';
 
 function week() {
     when = 'weekly';
-     document.getElementById('week').style.backgroundColor  = '#e9e9e9';
-    document.getElementById('month').style.backgroundColor = 'transparent';
-     document.getElementById('year').style.backgroundColor  = 'transparent';
+     document.getElementById('week').style.color = 'black';
+     document.getElementById('month').style.color = 'gray';
+     document.getElementById('year').style.color = 'gray';
+
 }
 function month() {
     when = 'monthly';
-    document.getElementById('month').style.backgroundColor = '#e9e9e9';
-     document.getElementById('week').style.backgroundColor  = 'transparent';
-     document.getElementById('year').style.backgroundColor  = 'transparent';
+    document.getElementById('month').style.color = 'black';
+    document.getElementById('week').style.color = 'gray';
+    document.getElementById('year').style.color = 'gray';
 }
 function year() {
     when = 'yearly';
-     document.getElementById('year').style.backgroundColor  = '#e9e9e9';
-     document.getElementById('week').style.backgroundColor  = 'transparent';
-    document.getElementById('month').style.backgroundColor = 'transparent';
+     document.getElementById('year').style.color = 'black';
+     document.getElementById('month').style.color = 'gray';
+     document.getElementById('week').style.color = 'gray';
 }
 month();
 
