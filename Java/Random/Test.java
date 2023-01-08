cd3d2c479320f4b6df336f346f83a1e63a26a011
Balazsmi <b.miskey2005@gmail.com>
2023-01-08 13:31:31 +0100

    random

--- Java/Random/Test.java
+++ Java/Random/Test.java
@@ -1,7 +1,23 @@
 package Java.Random;
-
+import java.util.*;
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Test");
+        reverse(5);
+    }
+
+
+    public static int[] reverse(int n){
+              
+        int[] array = new int[n];
+        int x = n;
+
+
+        for(int i = 0; i < n; i++) {
+          array[i] = x;
+          x--;
+          
+        }
+        System.out.println(Arrays.toString(array));
+        return array;
     }
 }
