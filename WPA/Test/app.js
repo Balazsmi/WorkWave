16fdb43264bf2e9284522ab404820c9675e434f2
Balazsmi <b.miskey2005@gmail.com>
2023-02-19 12:45:36 +0100

    new pwa

--- WPA/Test/app.js
+++ WPA/Test/app.js
@@ -1,10 +1,48 @@
-function show(id) {
-  var contents = document.getElementsByClassName("content");
-  for (var i = 0; i < contents.length; i++) {
-    if (contents[i].getAttribute("id") === id) {
-      contents[i].classList.add("active");
-    } else {
-      contents[i].classList.remove("active");
-    }
-  }
+const listIcon = document.getElementById("list-icon");
+const homeIcon = document.getElementById("home-icon");
+const createIcon = document.getElementById("create-icon");
+
+const listContent = document.getElementById("list");
+const homeContent = document.getElementById("home");
+const createContent = document.getElementById("create");
+
+homeIcon.classList.add("active");
+homeContent.classList.add("active");
+
+listIcon.addEventListener("click", function() {
+  homeIcon.classList.remove("active");
+  createIcon.classList.remove("active");
+  listIcon.classList.add("active");
+  
+  homeContent.classList.remove("active");
+  createContent.classList.remove("active");
+  listContent.classList.add("active");
+});
+
+homeIcon.addEventListener("click", function() {
+  listIcon.classList.remove("active");
+  createIcon.classList.remove("active");
+  homeIcon.classList.add("active");
+  
+  listContent.classList.remove("active");
+  createContent.classList.remove("active");
+  homeContent.classList.add("active");
+});
+
+createIcon.addEventListener("click", function() {
+  listIcon.classList.remove("active");
+  homeIcon.classList.remove("active");
+  createIcon.classList.add("active");
+  
+  listContent.classList.remove("active");
+  homeContent.classList.remove("active");
+  createContent.classList.add("active");
+});
+
+
+function showHomeContent() {
+  var homeContent = document.getElementById("homeContent");
+  homeContent.style.display = "block";
 }
+
+showHomeContent();
