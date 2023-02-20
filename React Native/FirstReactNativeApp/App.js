9c6b8f71ffefc619a4baa3759c225e6234126748
Balazsmi <b.miskey2005@gmail.com>
2023-02-20 18:10:41 +0100

    touchableHighlight

--- React Native/FirstReactNativeApp/App.js
+++ React Native/FirstReactNativeApp/App.js
@@ -1,13 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
+import { StyleSheet, Text,TouchableHighlight, TouchableWithoutFeedback, View, Image, SafeAreaView } from 'react-native';
 
 export default function App() {
   const handlePress = () => console.log("Text clicked")
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text onPress={handlePress}>Testing the App</Text>
-      <Image source={{uri: "https://picsum.photos/200/300"}}/>
+      <Text onPress={handlePress} style={styles.text}>Testing the App</Text>
+      <TouchableHighlight onPress={() => console.log("Image tapped")}>
+        <Image style={styles.image} fadeDuration={1000} source={{
+          width: 200,
+          height:300,
+          uri: "https://picsum.photos/200/300"}}/>
+      </TouchableHighlight>
     </SafeAreaView>
   );
 }
@@ -19,4 +24,12 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center'
   },
+
+  text: {
+    backgroundColor: 'gray',
+  },
+  image: {
+  },
+
+
 });
