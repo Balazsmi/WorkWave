679b68681ddd84ded9363f0f9765a99fb4acd577
Balazsmi <b.miskey2005@gmail.com>
2023-02-25 18:02:34 +0100

    added textInput and button

--- React Native/Tudu/App.js
+++ React Native/Tudu/App.js
@@ -9,14 +9,14 @@ export default function App() {
     <View style={styles.container}>
 
 
-      <View>
-        <TextInput style={styles.text} placeholder='Add a tudu'/>
-        <Button style={styles.add_button} title='+'></Button>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder='Add a task' placeholderTextColor="#9d9d9f" />
+        <Button style={styles.addButton} title='Add'></Button>
       </View>
 
 
       <View>
-        <Text>Your tudus</Text>
+        <Text style={styles.text}></Text>
       </View>
 
 
@@ -30,16 +30,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 150,
-    backgroundColor: '#121212',
+    padding: 50,
+    paddingTop: 75,
+    backgroundColor: '#1c1c1e',
     alignItems: 'center',
-
+  },
+  textInput: {
+    color: 'white',
+    width: '95%',
+    padding: 5,
+    fontSize: 20,
+    borderWidth: 1,
+    borderColor: '#9d9d9f',
+    borderRadius: 5,
+    marginRight: 5,
   },
   text: {
     color: 'white',
-    fontSize: 20,
   },
-  add_button: {
-    fontSize: 150,
+  addButton: {
+
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   }
 });
