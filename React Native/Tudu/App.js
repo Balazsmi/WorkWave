0a1b3d6e4bea1dfde60877e37475bbbe88949488
Balazsmi <b.miskey2005@gmail.com>
2023-02-25 17:39:52 +0100

    Tudu basic components added

--- React Native/Tudu/App.js
+++ React Native/Tudu/App.js
@@ -1,20 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
+
+
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+
+      <View>
+        <TextInput style={styles.text} placeholder='Add a tudu'/>
+        <Button style={styles.add_button} title='+'></Button>
+      </View>
+
+
+      <View>
+        <Text>Your tudus</Text>
+      </View>
+
+
     </View>
+
+
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    paddingTop: 150,
+    backgroundColor: '#121212',
     alignItems: 'center',
-    justifyContent: 'center',
+
   },
+  text: {
+    color: 'white',
+    fontSize: 20,
+  },
+  add_button: {
+    fontSize: 150,
+  }
 });
