419905512fbf48b9bcd4f3c702115d276a2119c6
Balazsmi <b.miskey2005@gmail.com>
2023-04-22 17:42:48 +0200

    kleine fixes title bei work und longBreak falsch

--- JavaScript/Pomodoro/main.js
+++ JavaScript/Pomodoro/main.js
@@ -91,7 +91,7 @@ function setWork() {
 		startingMinutes = document.getElementById("workInput").value;
 		(startingMinutes < 10)? document.querySelector("#minutes").innerHTML = '0' + startingMinutes  :  document.querySelector("#minutes").innerHTML = startingMinutes;
 		document.querySelector("#seconds").innerHTML = '00';
-		document.querySelector("#title").innerHTML = startingMinutes + ' : ' + '00';
+		(startingMinutes < 10)? document.querySelector("#title").innerHTML = '0' + startingMinutes + ':' + '00'  :  document.querySelector("#title").innerHTML = startingMinutes;
 	} else {
 		return;
 	}
@@ -103,7 +103,7 @@ function setShortBreak() {
 		startingMinutes = document.getElementById("shortBreakInput").value;
 		(startingMinutes < 10)? document.querySelector("#minutes").innerHTML = '0' + startingMinutes  :  document.querySelector("#minutes").innerHTML = startingMinutes;
 		document.querySelector("#seconds").innerHTML = '00';
-		document.querySelector("#title").innerHTML = startingMinutes + ' : ' + '00';
+		(startingMinutes < 10)? document.querySelector("#title").innerHTML = '0' + startingMinutes + ':' + '00'  :  document.querySelector("#title").innerHTML = startingMinutes;
 	} else {
 		return;
 	}
@@ -115,7 +115,7 @@ function setLongBreak() {
 		startingMinutes = document.getElementById("longBreakInput").value;
 		(startingMinutes < 10)? document.querySelector("#minutes").innerHTML = '0' + startingMinutes  :  document.querySelector("#minutes").innerHTML = startingMinutes;
 		document.querySelector("#seconds").innerHTML = '00';
-		document.querySelector("#title").innerHTML = startingMinutes + ' : ' + '00';
+		(startingMinutes < 10)? document.querySelector("#title").innerHTML = '0' + startingMinutes + ':' + '00'  :  document.querySelector("#title").innerHTML = startingMinutes;
 	} else {
 		return;
 	}
